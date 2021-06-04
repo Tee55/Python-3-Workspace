e39205684e8c67137369df05b36d01b7e9419ca0
Tee55 <teerapaths@gmail.com>
2021-06-04 21:54:04 +0800

    master

--- covid.py
+++ covid.py
@@ -10,7 +10,17 @@ from sklearn.utils.class_weight import compute_class_weight
 import pandas as pd
 from sklearn.metrics import classification_report, confusion_matrix
 import seaborn as sns
+from tensorflow.keras.applications.vgg19 import VGG19
+from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg19 import preprocess_input
 
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.pipeline import Pipeline
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.cluster import KMeans
 
 #Define Directories for train, test & Validation Set
 train_path = 'dataset/train'
@@ -19,8 +29,13 @@ valid_path = 'dataset/val'
 
 batch_size = 10
 
+'''
 img_height = 299
 img_width = 299
+'''
+
+img_height = 224
+img_width = 224
 
 def main():
 
@@ -36,7 +51,7 @@ def main():
     early = EarlyStopping(monitor='val_loss', mode='min', patience=3)
     learning_rate_reduction = ReduceLROnPlateau(monitor='val_loss', patience=2, verbose=1, factor=0.3, min_lr=0.000001)
 
-    callbacks_list = [ early, learning_rate_reduction]
+    callbacks_list = [early, learning_rate_reduction]
 
     weights = compute_class_weight('balanced', np.unique(train.classes), train.classes)
     cw = dict(zip( np.unique(train.classes), weights))
@@ -57,6 +72,31 @@ def main():
     sns.heatmap(cm, annot=True, fmt="d")
 
     plt.show()
+
+def decom():
+
+    image_gen = ImageDataGenerator(rescale = 1./255, shear_range = 0.2, zoom_range = 0.2, horizontal_flip = True)
+    test_data_gen = ImageDataGenerator(rescale = 1./255)
+
+    train = image_gen.flow_from_directory(train_path, target_size=(img_height, img_width), color_mode='rgb', class_mode='binary', batch_size=batch_size)
+    test = test_data_gen.flow_from_directory(test_path, target_size=(img_height, img_width), color_mode='rgb', shuffle=False, class_mode='binary', batch_size=batch_size)
+    valid = test_data_gen.flow_from_directory(valid_path, target_size=(img_height, img_width), color_mode='rgb', class_mode='binary', batch_size=batch_size)
+
+    decom = create_decomp()
+
+    features = decom.predict(train)
+
+    pca = PCA()
+
+    # prepare transform on dataset
+    pca.fit(features)
+
+    # apply transform to dataset
+    transformed = pca.transform(features)
+
+    print(transformed.shape)
+
+    k_meanClus(transformed)
 
 def create_model():
 
@@ -80,6 +120,37 @@ def create_model():
 
     return cnn
 
+def create_decomp():
+
+    model = VGG19(weights='imagenet')
+
+    return model
+
+def k_meanClus(X):
+    '''
+    wcss = []
+
+    for i in range(1, 11):
+        kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
+        kmeans.fit(X)
+        wcss.append(kmeans.inertia_)
+
+    plt.plot(range(1, 11), wcss)
+    plt.title('Elbow Method')
+    plt.xlabel('Number of clusters')
+    plt.ylabel('WCSS')
+    plt.show()
+    '''
+
+    kmeans = KMeans(n_clusters=4, init='k-means++', max_iter=300, n_init=10, random_state=0)
+    pred_y = kmeans.fit_predict(X)
+    plt.scatter(X[:,0], X[:,1])
+    plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='red')
+    plt.show()
+
+    print(pred_y)
+
 if __name__ == '__main__':
-    main()
+    #main()
+    decom()
     
